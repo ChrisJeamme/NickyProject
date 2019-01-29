0c899f22a2cdd78e6a3d8f8c6ace7939ac8f7d20
dimitribruyere <dimitri.bruyere@outlook.fr>
2019-01-29 17:06:39 +0100

    funnctions for lemmatization

---
+++ project/project/lem.py
@@ -0,0 +1,24 @@
+from nltk.corpus import wordnet
+
+def get_wordnet_pos(treebank_tag):
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return None 
+
+def lemmatize_tagged_token(token):
+    word = token[0]
+    tag = token[1]
+    wntag = get_wordnet_pos(tag)
+    if wntag is None:# not supply tag in case of None
+        return lemmatizer.lemmatize(word) 
+    else:
+        return lemmatizer.lemmatize(word, pos=wntag) 
+    
+# tokens = list(map(lemmatize_tagged_token, tagged))
