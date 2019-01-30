fe2838852d8b802c72aac4994136bad29d09f3fb
Drydge <antho.deveaux@gmail.com>
2019-01-30 07:24:41 +0100

    add pipelinize function

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from data_loader import Type
 
 import pandas as pd
 import numpy as np
+import re
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.linear_model import LogisticRegression
+import nltk
+
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.linear_model import SGDClassifier
+from sklearn.pipeline import Pipeline
+
+from sklearn.feature_extraction.text import TfidfTransformer,CountVectorizer
 
 dataload = DataLoader(Type.ARTICLE_SET)
 dataload.fit("./project/project/train.csv","./project/project/test.csv")
@@ -16,36 +26,50 @@ X_train=X_train[0:n]
 y_test=y_train[n:]
 y_train=y_train[0:n]
 
-from sklearn.feature_extraction.text import CountVectorizer
 count_vect = CountVectorizer()
 X_train_counts = count_vect.fit_transform(X_train)
 
-print(X_train_counts.toarray())
-print(count_vect.get_feature_names())
-print(X_train_counts.shape)
-print(count_vect.vocabulary_.get(u'dlrs'))
 
-
-from sklearn.feature_extraction.text import TfidfTransformer
-tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
+tf_transformer = TfidfTransformer(use_idf=True).fit(X_train_counts)
 X_train_tf = tf_transformer.transform(X_train_counts)
-print(X_train_tf[0])
+
 
 
 x_count = count_vect.transform(X_test)
 x_tf = tf_transformer.transform(x_count)
-print(y_test)
 
-from sklearn.linear_model import SGDClassifier
-from sklearn.pipeline import Pipeline
-text_clf = Pipeline([
-     ('vect', CountVectorizer()),
-     ('tfidf', TfidfTransformer()),
-     ('clf', SGDClassifier(loss='squared_hinge', penalty='l2',
+def pipelinize(function, active=True):
+    def list_comprehend_a_function(list_or_series, active=True):
+        if active:
+            return [function(i) for i in list_or_series]
+        else: # if it's not active, just pass it right back
+            return list_or_series
+    return FunctionTransformer(list_comprehend_a_function, validate=False, kw_args={'active':active})
+
+
+
+def rem_num(X):
+    for x in X:
+        x = re.sub('[0-9]*', '#num', x)
+    return X
+
+#from sklearn_helpers import train_test_and_evaluate
+
+tokenizer = nltk.casual.TweetTokenizer(preserve_case=False, reduce_len=True)
+count_vect = CountVectorizer(tokenizer=tokenizer.tokenize)     
+classifier = SGDClassifier(loss='squared_hinge', penalty='l2',
                            alpha=1e-3, random_state=42,
-                           max_iter=1000, tol=None)),])
+                           max_iter=10, tol=None)
+classifier = LogisticRegression()
+
+
+text_clf = Pipeline([
+     ('rem_num', pipelinize(rem_num)),
+     ('vect', count_vect),
+     ('tfidf', TfidfTransformer()),
+     ('clf', classifier),])
 
 text_clf.fit(X_train, y_train) 
-    
+#confusion_matrix = train_test_and_evaluate(text_clf, X_train, y_train, X_test, y_test)
 predicted = text_clf.predict(X_test)
-np.mean(predicted == y_test)
+print(np.mean(predicted == y_test))
